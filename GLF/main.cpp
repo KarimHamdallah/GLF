cf8f7fd85159839d70c4b0aac7509a59524d2311
KarimHamdallah <108572488+KarimHamdallah@users.noreply.github.com>
2026-02-07 03:15:16 +0200

    - add glad

--- GLF/main.cpp
+++ GLF/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <Math/Math.h>
 
 
 int main()
 {
 	glfwInit();
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello window", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
 	}
 
 	return 0;
